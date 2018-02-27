d0a1ddb47d333b18fc60d94d5454ae32daa17a09
Nathan Sullivan <nathan@nightsys.net>
2018-02-27 21:55:31 +1000

    fix go vet

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func parseTagEnvVars() (InstanceTags, error) {
 		if len(pair[0]) > 5 && pair[0][0:5] == "TAG__" {
 			split_key := strings.Split(pair[0], "__")
 			if len(split_key) != 3 {
-				return InstanceTags{}, fmt.Errorf("Invalid format for '%s' environment variable key. Must be in the format of 'TAG__instanceid__tagname'")
+				return InstanceTags{}, fmt.Errorf("Invalid format for '%s' environment variable key. Must be in the format of 'TAG__instanceid__tagname'", split_key)
 			}
 			use_instance_id := strings.Replace(split_key[1], "_", "-", 1)
 			result[use_instance_id] = append(result[use_instance_id], InstanceTag{
